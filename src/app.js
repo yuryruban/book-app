f53875be146820b77144b1f6c97eba1159319b0d
Iurii Ruban <iuriiruban@Iuriis-MacBook-Pro.local>
2023-11-13 17:34:08 +0400

    add states and onchange

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ class App{
 	routes = [
 		{path: "", view: MainView}
 	]
+
+	appState = {
+		favorites: []
+	}
+
+
 	constructor(){
 		window.addEventListener('hashchange',this.route.bind(this))
 		this.route()
@@ -16,7 +22,7 @@ class App{
 			this.currentView.destroy()
 		}
 		const view = this.routes.find(r=>r.path == location.hash).view
-		this.currentView = new view()
+		this.currentView = new view(this.appState)
 		this.currentView.render()
 	}
 }
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,15 +1,34 @@
 import { AbstractView } from '../../common/view.js'
+import onChange from 'on-change'
 
 export class MainView extends AbstractView{
-	constructor(){
+	state = {
+		list: [],
+		loading: false,
+		searchQuery: undefined,
+		offset: 0
+	}
+	
+
+	constructor(appState){
 		super()
+		this.appState = appState
+		this.appState = onChange(this.appState, this.appStateHook.bind(this))
 		this.setTitle('Поиск книг')
+	}
+
+	appStateHook(path){
+		console.log(path)
+		if(path === 'favorites'){
+			console.log(path)
+		}
 	}
 
 	render(){
 		const main = document.createElement('div')
-		main.innerHTML = 'Other test'
+		main.innerHTML = `Число книг: ${this.appState.favorites.length}`
 		this.app.innerHTML = ''
 		this.app.append(main)
+		this.appState.favorites.push('d')
 	}
 }
